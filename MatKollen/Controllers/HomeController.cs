07dadee2dc6e628ddaa8bc5b5438dd988a6ee889
Saga-Lannerhjelm <saga.lannerhjelm@icloud.com>
2024-10-11 17:54:06 +0200

    Small changes

--- MatKollen/Controllers/HomeController.cs
+++ MatKollen/Controllers/HomeController.cs
@@ -6,27 +6,51 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace MatKollen.Controllers;
 
+
+// https://learn.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-8.0
+public interface IFoodService{
+    public void AddUser();
+}
+
+public class FoodService : IFoodService{
+    public void AddUser(){
+        Console.Write("h"); //använder db
+    }
+}
+
+public class FakeFoodService : IFoodService{
+    public void AddUser(){
+        Console.Write("h"); // använder inte db
+    }
+}
+
+
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IFoodService _foodService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,FoodService foodService)
     {
         _logger = logger;
+        _foodService = foodService;
     }
+
+    [AllowAnonymous]
     public IActionResult Index()
     {
         var foodRep = new FoodRepository();
-        var categories = foodRep.GetData(out string error);
-
-        ViewBag.categories = categories;
+        // var categories = foodRep.GetData(out string error);
+        // _foodService.AddUser();
+        ViewBag.categories = "categories";
         return View();
     }
 
-    [Authorize]
     public IActionResult Privacy()
     {
         var currentUser = HttpContext.User;
+        var user = User;
+        ViewBag.Username = currentUser.Claims.FirstOrDefault(c => c.Type == "name")?.Value;
         return View();
     }
 
