17089465dc6071287dc913324061ef6d8ea0465a
Saga-Lannerhjelm <saga.lannerhjelm@icloud.com>
2024-10-14 13:51:27 +0200

    Declaring properties as nullable

--- MatKollen/ViewModels/RecipeDetailsViewModel.cs
+++ MatKollen/ViewModels/RecipeDetailsViewModel.cs
@@ -8,9 +8,9 @@ namespace MatKollen.ViewModels
 {
     public class RecipeDetailsViewModel
     {
-        public Recipe Recipe { get; set; }
-        public string Category { get; set; }
-        public string Username { get; set; }
+        public Recipe? Recipe { get; set; }
+        public string? Category { get; set; }
+        public string? Username { get; set; }
         public List<RecipeFoodItemViewModel> Ingredients { get; set; } = new List<RecipeFoodItemViewModel>();
     }
 }
