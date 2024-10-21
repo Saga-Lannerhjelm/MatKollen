00095f481e59155a2185975d9b288dc138d2fa25
Saga-Lannerhjelm <saga.lannerhjelm@icloud.com>
2024-10-21 08:54:27 +0200

    Add view for "My recipes"

--- MatKollen/DAL/Repositories/RecipeRepository.cs
+++ MatKollen/DAL/Repositories/RecipeRepository.cs
@@ -63,7 +63,56 @@ namespace MatKollen.DAL.Repositories
                     errorMsg = e.Message;
                     return null;
                 }    
+            }
+        }
 
+        public List<RecipeDetailsViewModel>? GetUsersRecipes(int userId, out string errorMsg)
+        {
+            var myConnectionString = _connectionString;
+
+            using (var myConnection = new MySqlConnection(myConnectionString))
+            {
+                string query  = "SELECT * FROM vw_recipe_details WHERE user_id = @userId";
+                var recipeList = new List<RecipeDetailsViewModel>();
+
+                try
+                {
+                    // create a MySQL command and set the SQL statement with parameters
+                    MySqlCommand myCommand = new MySqlCommand(query, myConnection);
+                    myConnection.Open();
+                    myCommand.Parameters.Add("@userId", MySqlDbType.Int32).Value = userId;
+
+                    errorMsg = "";
+
+                    // execute the command and read the results
+                    using var reader = myCommand.ExecuteReader();
+                    {
+                        while (reader.Read())
+                        {
+                            var recipe = new RecipeDetailsViewModel()
+                            {
+                                Recipe = new Recipe()
+                                {
+                                    Id = reader.GetInt32("id"),
+                                    Title = reader.GetString("title"),
+                                    Description = reader.GetString("description"),
+                                    CreatedAt = reader.GetDateTime("created_at"),
+                                    RecipeCategoryId = reader.GetInt32("recipe_category_id"),
+                                    UserId = reader.GetInt32("user_id")
+                                },
+                                Category = reader.GetString("category"),
+                                Username = reader.GetString("username"),
+                            }; 
+                            recipeList.Add(recipe);
+                        }
+                    }
+                    return recipeList;
+                }
+                catch (MySqlException e)
+                {
+                    errorMsg = e.Message;
+                    return null;
+                }    
             }
         }
 
@@ -108,18 +157,21 @@ namespace MatKollen.DAL.Repositories
                                 recipe.Username = reader.GetString("username"); 
                             }
 
-                            recipe.Ingredients.Add(new RecipeIngredientViewModel()
+                            if (!reader.IsDBNull(reader.GetOrdinal("quantity")))
                             {
-                                IngredientDetails = new RecipeFoodItem()
+                                recipe.Ingredients.Add(new RecipeIngredientViewModel()
                                 {
-                                    Quantity = reader.GetDouble("quantity"),
-                                    UnitId =  reader.GetInt32("unit_id"),
-                                    FoodItemId = reader.GetInt32("food_item_id")
-                                },
-                                ConvertedQuantity = conversionHandler.ConverFromtLiterOrKg(reader.GetDouble("quantity"), reader.GetDouble("conversion_multiplier")),
-                                Ingredient = reader.GetString("ingredient"),
-                                Unit = reader.GetString("unit")
-                            });
+                                    IngredientDetails = new RecipeFoodItem()
+                                    {
+                                        Quantity = reader.GetDouble("quantity"),
+                                        UnitId =  reader.GetInt32("unit_id"),
+                                        FoodItemId = reader.GetInt32("food_item_id")
+                                    },
+                                    ConvertedQuantity = conversionHandler.ConverFromtLiterOrKg(reader.GetDouble("quantity"), reader.GetDouble("conversion_multiplier")),
+                                    Ingredient = reader.GetString("ingredient"),
+                                    Unit = reader.GetString("unit")
+                                }); 
+                            }
                         }
                     }
                     return recipe;
@@ -129,8 +181,8 @@ namespace MatKollen.DAL.Repositories
                     errorMsg = e.Message;
                     return null;
                 }    
-
             }
         }
+
     }
 }
