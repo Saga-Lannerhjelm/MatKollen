94badd65ee9aeca5cc33ece3b566f6eadf4555a2
Saga-Lannerhjelm <saga.lannerhjelm@icloud.com>
2024-10-26 12:41:16 +0200

    Model validation and display "1st" without the quantity and unit

--- MatKollen/Models/GroceryListFoodItem.cs
+++ MatKollen/Models/GroceryListFoodItem.cs
@@ -9,11 +9,11 @@ namespace MatKollen.Models
     public class GroceryListFoodItem
     {
         public int Id { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Välj vilken mängd av matvaran du vill ha")]
         public decimal Quantity { get; set; }
         [Range(1, int.MaxValue, ErrorMessage = "Du måste välja en enhet")]
         public int UnitId { get; set; }
         public int ListId { get; set; } 
-        // [Range(1, int.MaxValue, ErrorMessage = "Du måste välja en vara")]
         public int FoodItemId { get; set; }
         public bool Completed { get; set; }
     }
