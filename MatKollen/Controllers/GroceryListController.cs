0bb0d7d9fb999486bea98cc9745b361c115930f1
Saga-Lannerhjelm <saga.lannerhjelm@icloud.com>
2024-10-30 15:26:41 +0100

    cleanup GroceryListController

--- MatKollen/Controllers/GroceryListController.cs
+++ MatKollen/Controllers/GroceryListController.cs
@@ -33,18 +33,81 @@ namespace MatKollen.Controllers
             _convertQuantityHandler = convertQuantityHandler;
         }
 
-        //GroceryList
         public IActionResult Index()
         {
             int userId = UserHelper.GetUserId(User);
-            var groceryItems = _groceryListRepository.GetGroceryList(userId, out string error);
+            var groceryListItems = _groceryListRepository.GetGroceryList(userId, out string error);
 
             if (error != "")
             {
                 TempData["error"] = error;
             }
 
-            return View(groceryItems);
+            return View(groceryListItems);
+        }
+
+        [HttpGet]
+        public IActionResult AddItemToGroceryList()
+        {
+            var foodItem = new GroceryListFoodItem();
+            GetLists();
+            return View(foodItem);
+        }
+
+        [HttpPost]
+        public IActionResult AddItemToGroceryList(GroceryListFoodItem item)
+        {
+            if (item.FoodItemId == 0)
+            {
+                ModelState.AddModelError(nameof(item.FoodItemId), "Välj en matvara");
+            }
+            if (!ModelState.IsValid)
+            {
+                GetLists();
+                return View(item);
+            }
+
+            var unitList = _unitRepository.GetUnits(out string unitsError);
+            if (unitsError != "")
+            {
+                TempData["error"] = unitsError;
+                return View(item);
+            }
+            // Find measurement unit based on unit id
+            var unitInfo = unitList.Find(m => m.Id == item.UnitId);
+            // Recalculate quantity
+            item.Quantity = _convertQuantityHandler.ConverToLiterOrKg(item.Quantity, unitInfo.Multiplier);
+
+            var groceryListFoodItem = new GroceriesToAddViewModel(){
+                ListItem = item,
+                UnitType = unitInfo.Type
+            };
+
+            //Get user id
+            int userId = UserHelper.GetUserId(User);
+            // Insert values in database
+            int affectedRows = _groceryListRepository.InsertOrUpdateFoodItems(groceryListFoodItem, userId, out string error);
+
+            if (error != "")
+            {
+                TempData["error"] = "Det gick inte att lägga till matvaran:" + error;
+            }
+
+            if (affectedRows == 0) TempData["error"] = "Ingen matvara har lagts till";
+            else TempData["success"] = "Matvara tillagd!";
+
+            return RedirectToAction("Index");
+        }
+
+        private void GetLists()
+        {
+            List<MeasurementUnit> unitList = _unitRepository.GetUnits(out string unitsError);
+            List<FoodItem>? foodItemList = _foodRepository.GetFoodItems(out string error);
+
+            if (unitsError != "" || error != "") TempData["error"] = unitsError + " " + error;
+
+            ViewData["units"] = unitList;
+            ViewData["foodItems"] = foodItemList;
         }
 
         [HttpPost]
@@ -73,18 +136,16 @@ namespace MatKollen.Controllers
                     
                     var affectedRows = _userFoodItemRepository.AddFoodItem(userFoodItem, out string insertError);
 
-                    if (insertError != "")
+                    if (insertError != "" || affectedRows == 0)
                     {
-                        TempData["error"] = error;
+                        TempData["error"] = "Gick inte att lägga till alla matvaror." + insertError;
+                        return RedirectToAction("Index");
                     }
 
-                    if (affectedRows > 0)
+                    _groceryListRepository.Delete(food.Id, out string deleteError);
+                    if (deleteError != "")
                     {
-                        _groceryListRepository.Delete(food.Id, out string deleteError);
-                        if (deleteError != "")
-                        {
-                            TempData["error"] = error;
-                        }
+                        TempData["error"] = error;
                     }
                 }
             }
@@ -92,74 +153,7 @@ namespace MatKollen.Controllers
             return RedirectToAction("index", "UserFoodItems");
         }
 
-        //GroceryList/AddItem
-        [HttpGet]
-        public IActionResult AddItemToGroceryList()
-        {
-            var foodItem = new GroceryListFoodItem();
-            GetLists();
-            return View(foodItem);
-        }
-
         [HttpPost]
-        public IActionResult AddItemToGroceryList(GroceryListFoodItem item)
-        {
-            if (item.FoodItemId == 0)
-            {
-                ModelState.AddModelError(nameof(item.FoodItemId), "Välj en matvara");
-            }
-            if (!ModelState.IsValid)
-            {
-                GetLists();
-                return View(item);
-            }
-
-            // Find multiplier based on unit id
-            var unitList= _unitRepository.GetUnits(out string unitsError);
-            if (unitsError != "")
-            {
-                TempData["error"] = unitsError;
-                return View(item);
-            }
-            var unitInfo = unitList.Find(m => m.Id == item.UnitId);
-
-            var groceryItem = new GroceriesToAddViewModel(){
-                ListItem = item,
-                UnitType = unitInfo.Type
-            };
-
-            //Get user id
-            int userId = UserHelper.GetUserId(User);
-
-            // Recalculate quantity
-            item.Quantity = _convertQuantityHandler.ConverToLiterOrKg(item.Quantity, unitInfo.Multiplier);
-
-            // Insert values in database
-            int affectedRows = _groceryListRepository.InsertOrUpdateFoodItems(groceryItem, userId, out string error);
-
-            if (error != "")
-            {
-                TempData["error"] = "Det gick inte att lägga till matvaran:" + error;
-            }
-
-            if (affectedRows == 0) TempData["error"] = "Det gick inte att lägga till matvaran";
-            else TempData["success"] = "Matvara tillagd!";
-
-            return RedirectToAction("Index");
-        }
-
-        private void GetLists()
-        {
-            List<MeasurementUnit> unitList = _unitRepository.GetUnits(out string unitsError);
-            List<FoodItem>? foodItemList = _foodRepository.GetFoodItems(out string error);
-
-            if (unitsError != "" || error != "") TempData["error"] = unitsError + " " + error;
-
-            ViewData["units"] = unitList;
-            ViewData["foodItems"] = foodItemList;
-        }
-
-         [HttpPost]
         public IActionResult AddFromRecipe(List<int> checkedItems)
         {
             int userId = UserHelper.GetUserId(User);
@@ -179,6 +173,18 @@ namespace MatKollen.Controllers
         }
 
         [HttpPost]
+        public IActionResult MarkAsComplete(int id)
+        {
+            var isCompleted = _groceryListRepository.UpdateCompletedState(id, out string error);
+            if (error != "")
+            {
+                TempData["error"] = error;
+            }
+
+            return new JsonResult(Ok(isCompleted));
+        }
+        
+        [HttpPost]
         public IActionResult Delete(int id)
         {
             var rowsAffected = _groceryListRepository.Delete(id, out string error);
@@ -192,18 +198,6 @@ namespace MatKollen.Controllers
             }
 
             return RedirectToAction("Index");
-        }
-
-        [HttpPost]
-        public IActionResult MarkAsComplete(int id)
-        {
-            var isCompleted = _groceryListRepository.UpdateCompletedState(id, out string error);
-            if (error != "")
-            {
-                TempData["error"] = error;
-            }
-
-            return new JsonResult(Ok(isCompleted));
         }
     }
 }
