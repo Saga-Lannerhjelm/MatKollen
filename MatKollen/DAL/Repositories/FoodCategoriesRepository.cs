768926ace5e5034a59f81cb642b98f90096869fe
Saga-Lannerhjelm <saga.lannerhjelm@icloud.com>
2024-10-31 09:06:31 +0100

    Remove unused method

--- MatKollen/DAL/Repositories/FoodCategoriesRepository.cs
+++ MatKollen/DAL/Repositories/FoodCategoriesRepository.cs
@@ -50,44 +50,6 @@ namespace MatKollen.DAL.Repositories
                     return null;
                 }    
             }
-        }
-
-        public List<RecipeCategory> GetRecipeCategories(out string errorMsg)
-        {
-            var myConnectionString = _connectionString;
-            string query  = "SELECT id, name FROM recipe_categories";
-            List<RecipeCategory> categoryList = [];
-
-            using (var myConnection = new MySqlConnection(myConnectionString))
-            {
-                try
-                {
-                    MySqlCommand myCommand = new MySqlCommand(query, myConnection);
-                    myConnection.Open();
-                    errorMsg = "";
-
-                    using var reader = myCommand.ExecuteReader();
-                    {
-                        while (reader.Read())
-                        {
-                            var category = new RecipeCategory()
-                            {
-                                Id = reader.GetInt32("id"),
-                                Name = reader.GetString("name"),
-                            };
-                            categoryList.Add(category);
-                        }
-                    }
-                    return categoryList;
-                }
-                catch (MySqlException e)
-                {
-                    errorMsg = e.Message;
-                    return null;
-                }    
-            }
-        }
-
-        
+        }        
     }
 }
